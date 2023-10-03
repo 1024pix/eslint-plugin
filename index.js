99772d16577bba484c6f851b6b5b6c12f5d10e48
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-10-03 10:42:51 +0200

    :tada: Start project

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+const noSinonStubWithArgsOneliner = require('./rules/no-sinon-stub-with-args-oneliner.js');
+
+module.exports = {
+  rules: { 'no-sinon-stub-with-args-oneliner': noSinonStubWithArgsOneliner },
+};
