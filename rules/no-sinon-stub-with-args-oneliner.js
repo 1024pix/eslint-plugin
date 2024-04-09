fc45415ad3c169d3791f9a950200d7d2e6a84129
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-04-09 14:57:54 +0200

    Bump ESLint 9 and switch to ESM

--- rules/no-sinon-stub-with-args-oneliner.js
+++ rules/no-sinon-stub-with-args-oneliner.js
@@ -1,5 +1,3 @@
-'use strict';
-
 function report(context, node) {
   context.report({
     node: node,
@@ -7,36 +5,32 @@ function report(context, node) {
   });
 }
 
-module.exports = {
+export default {
   meta: {
     type: 'problem',
     docs: {
-      description:
-        'Do not chain `sinon.stub()` with the `withArgs()` method in a one-liner',
+      description: 'Do not chain `sinon.stub()` with the `withArgs()` method in a one-liner',
     },
     messages: {
-      chainError:
-        '`sinon.stub()` should not be chained with the `withArgs` method.',
+      chainError: '`sinon.stub()` should not be chained with the `withArgs` method.',
     },
   },
-  create: function (context) {
-    return {
-      'VariableDeclarator > CallExpression > MemberExpression[property.name="returns"] > CallExpression > MemberExpression[property.name="withArgs"] > CallExpression > MemberExpression[property.name="stub"][object.name="sinon"]':
-        function (node) {
-          report(context, node);
-        },
-      'VariableDeclarator > CallExpression > MemberExpression[property.name="throws"] > CallExpression > MemberExpression[property.name="withArgs"] > CallExpression > MemberExpression[property.name="stub"][object.name="sinon"]':
-        function (node) {
-          report(context, node);
-        },
-      'VariableDeclarator > CallExpression > MemberExpression[property.name="resolves"] > CallExpression > MemberExpression[property.name="withArgs"] > CallExpression > MemberExpression[property.name="stub"][object.name="sinon"]':
-        function (node) {
-          report(context, node);
-        },
-      'VariableDeclarator > CallExpression > MemberExpression[property.name="rejects"] > CallExpression > MemberExpression[property.name="withArgs"] > CallExpression > MemberExpression[property.name="stub"][object.name="sinon"]':
-        function (node) {
-          report(context, node);
-        },
-    };
-  },
+  create: (context) => ({
+    'VariableDeclarator > CallExpression > MemberExpression[property.name="returns"] > CallExpression > MemberExpression[property.name="withArgs"] > CallExpression > MemberExpression[property.name="stub"][object.name="sinon"]':
+      (node) => {
+        report(context, node);
+      },
+    'VariableDeclarator > CallExpression > MemberExpression[property.name="throws"] > CallExpression > MemberExpression[property.name="withArgs"] > CallExpression > MemberExpression[property.name="stub"][object.name="sinon"]':
+      (node) => {
+        report(context, node);
+      },
+    'VariableDeclarator > CallExpression > MemberExpression[property.name="resolves"] > CallExpression > MemberExpression[property.name="withArgs"] > CallExpression > MemberExpression[property.name="stub"][object.name="sinon"]':
+      (node) => {
+        report(context, node);
+      },
+    'VariableDeclarator > CallExpression > MemberExpression[property.name="rejects"] > CallExpression > MemberExpression[property.name="withArgs"] > CallExpression > MemberExpression[property.name="stub"][object.name="sinon"]':
+      (node) => {
+        report(context, node);
+      },
+  }),
 };
