d1d2544887feefd0fbcdd4a46416be7bb1f68b4a
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-05-21 16:55:27 +0200

    feat: expose flat config

---
+++ config.js
@@ -0,0 +1,108 @@
+const { resolve } = require('node:path');
+
+const js = require('@eslint/js');
+const { fixupPluginRules } = require('@eslint/compat');
+const comments = require('@eslint-community/eslint-plugin-eslint-comments/configs');
+const eslintPluginYml = require('eslint-plugin-yml');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
+const i18nJson = require('eslint-plugin-i18n-json');
+const pixPlugin = require('./index.js');
+
+module.exports = [
+  js.configs.recommended,
+  ...eslintPluginYml.configs['flat/standard'],
+  comments.recommended,
+  {
+    plugins: {
+      'i18n-json': fixupPluginRules(i18nJson),
+      '@1024pix': pixPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'arrow-parens': ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'computed-property-spacing': ['error', 'never'],
+      'eol-last': ['error'],
+      '@eslint-community/eslint-comments/no-unused-disable': ['error'],
+      indent: [
+        'error',
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
+      'keyword-spacing': ['error'],
+      'linebreak-style': ['error', 'unix'],
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1,
+          maxEOF: 1,
+        },
+      ],
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector:
+            'NewExpression[callee.name=Date][arguments.length=1][arguments.0.type=Literal]:not([arguments.0.value=/^[12][0-9]{3}-(0[0-9]|1[0-2])-([0-2][0-9]|3[01])(T([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]Z)?$/])',
+          message: "Use only ISO8601 UTC syntax ('2019-03-12T01:02:03Z') in Date constructor",
+        },
+        {
+          selector:
+            "CallExpression[callee.object.object.name='faker'][callee.object.property.name='internet'][callee.property.name='email']",
+          message: 'Use only faker.internet.exampleEmail()',
+        },
+      ],
+      'no-restricted-globals': [
+        'error',
+        {
+          name: 'fetch',
+          message: "Use import fetch from 'fetch'",
+        },
+      ],
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '_',
+        },
+      ],
+      'no-var': ['error'],
+      'object-curly-spacing': ['error', 'always'],
+      'prefer-const': ['error'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'space-before-blocks': ['error'],
+      'space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'never',
+          named: 'never',
+          asyncArrow: 'ignore',
+        },
+      ],
+      'space-in-parens': ['error'],
+      'space-infix-ops': ['error'],
+      'func-call-spacing': ['error'],
+      'key-spacing': ['error'],
+      'comma-spacing': ['error'],
+      'no-trailing-spaces': ['error'],
+      'no-multi-spaces': ['error'],
+      'yml/quotes': [
+        'error',
+        {
+          prefer: 'single',
+          avoidEscape: true,
+        },
+      ],
+      'i18n-json/sorted-keys': [
+        'error',
+        {
+          sortFunctionPath: resolve(__dirname, './json/sort-translations.js'),
+        },
+      ],
+      '@1024pix/no-sinon-stub-with-args-oneliner': 'error',
+    },
+  },
+];
