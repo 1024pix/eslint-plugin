9cedb54a3c87918ff58f9cff7c27263f62fdae12
Vincent Hardouin <vincenthardouin@icloud.com>
2024-07-16 17:59:23 +0200

    Revert "[FEATURE] Ajout d'une règle forcant l'ajout d'un commentaire sur les colonnes."

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 const noSinonStubWithArgsOneliner = require('./rules/no-sinon-stub-with-args-oneliner.js');
-const noColumnMigrationWithoutComment = require('./rules/no-column-migration-without-comment.js');
 
 module.exports = {
-  rules: { 'no-sinon-stub-with-args-oneliner': noSinonStubWithArgsOneliner, 'no-column-migration-without-comment': noColumnMigrationWithoutComment },
+  rules: { 'no-sinon-stub-with-args-oneliner': noSinonStubWithArgsOneliner },
 };
